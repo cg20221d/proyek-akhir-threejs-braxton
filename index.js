609e490ac2cfea07164ba24497aece971ad5f330
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-11-30 10:53:57 +0700

    add orbit controls

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import * as THREE from './three.js-master/build/three.module.js';
 import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
 const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
-
-
 
 const loaders = new GLTFLoader();
 loaders.load('../assets/house.glb', function (glb) {
   console.log(glb);
   const root = glb.scene;
-  root.scale.set(0.2, 0.2, 0.01);
+  root.scale.set(0.2, 0.2, 0.2);
   scene.add(root);
 }, function (xhr) {
   console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -26,15 +25,9 @@ const sizes = {
   height: window.innerHeight
 }
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); // red
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
-
-
-
 const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0, 1, 2);
+camera.position.set(0, 0.5, 2);
+camera.position.z = 2;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -51,3 +44,29 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate()
+
+// make scene rotate
+function onDocumentMouseMove(event) {
+  mouseX = (event.clientX - windowHalfX);
+  mouseY = (event.clientY - windowHalfY);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+window.addEventListener('resize', onWindowResize);
+
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 5;
+controls.maxPolarAngle = Math.PI / 2;
+
+
+
